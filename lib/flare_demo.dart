40d5a3e361021d089ff9c94d415b5c3a6076d3b9
Thandohimself <thandohimself@gmail.com>
2019-12-10 16:49:18 +0200

    Initial commit

---
+++ lib/flare_demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class FlareDemo extends StatefulWidget {
+  @override
+  _FlareDemoState createState() => _FlareDemoState();
+}
+
+class _FlareDemoState extends State<FlareDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'flare_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flare demo',
+      theme: ThemeData(
+        primaryColorDark: Colors.amber,
+      ),
+      home: FlareDemo()
+    );
+  }
+}
