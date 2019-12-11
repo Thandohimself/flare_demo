d1a434cd203ca34afbf1ee8a00fb345a8ade62bf
ThandoHimself <thando@immedia.co.za>
2019-12-11 09:38:42 +0200

    added animation

--- lib/flare_demo.dart
+++ lib/flare_demo.dart
@@ -1,4 +1,6 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FlareDemo extends StatefulWidget {
   @override
@@ -6,8 +8,22 @@ class FlareDemo extends StatefulWidget {
 }
 
 class _FlareDemoState extends State<FlareDemo> {
+  bool isOpen = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
+        child: FlareActor('assets/button-animation.flr',
+            animation: isOpen ? 'activate' : 'deactivate',
+        ),
+      ),
+    );
   }
 }
